8d81f01dec564a7d4a914de1f573e8a9020b540b
awarkopyr <awarkohm2002@gmail.com>
2024-04-07 21:49:22 +0600

    at least

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
@@ -19,12 +19,12 @@ const userSchema = new Schema(
             lowercase: true,
             trim: true,
         },
-        fullname: {
+        fullName: {
             type: String,
             required: true,
             trim: true,
         },
-        avator: {
+        avatar: {
             type: String, ///clouding url
             required: true,
         },
@@ -50,9 +50,9 @@ const userSchema = new Schema(
     }
 )
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) return next;
+    if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -62,7 +62,7 @@ userSchema.methods.generateAccessToken = function () {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullname: this.fullname
+            fullName: this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -1,5 +1,5 @@
-import { v2 as cloudinary } from cloudinary
-import fs from fs
+import { v2 as cloudinary } from "cloudinary"
+import fs from "fs"
 
 cloudinary.config({
     cloud_name: process.env.FILE_CLOUD_NAME,
